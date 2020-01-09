4009344e0b6d43398d01e5b097e2d5c21a63de84
Timo Glastra <timo@glastra.me>
2020-01-09 16:48:23 +0100

    build: allow deps-dev and dependencies as commitlint scopes

---
+++ .commitlintrc.js
@@ -0,0 +1,16 @@
+const { utils } = require('@commitlint/config-lerna-scopes')
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [2, 'always', 'kebab-case'],
+    'scope-enum': ctx =>
+      utils
+        .getPackages(ctx)
+        .then(packages => [
+          2,
+          'always',
+          [...packages, 'deps-dev', 'dependencies']
+        ])
+  }
+}
