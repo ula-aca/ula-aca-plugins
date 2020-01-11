02dbf99f1a2f2e47c4ff48d72850711d0b76419d
Timo Glastra <timo@glastra.me>
2020-01-11 16:51:55 +0100

    test(schema): added and refactored tests

--- packages/schema/tests/SchemaController.spec.ts
+++ packages/schema/tests/SchemaController.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 /*
  * Copyright 2020  ula-aca.
  *
@@ -18,6 +17,7 @@
 import { EventHandler, Message, UlaResponse } from 'universal-ledger-agent'
 import sinon from 'sinon'
 import { SchemaApi } from '@ula-aca/aries-cloudagent-interface'
+import { stubInterfaceFunction } from '@ula-aca/test-utils'
 import {
   SchemaController,
   GetCreatedSchemasMessage,
@@ -25,62 +25,28 @@ import {
   GetSchemaByIdMessage
 } from '../src'
 
-function stubInterfaceFunction({
-  Class,
-  functionName,
-  status = 200,
-  data = undefined
-}): sinon.SinonStub {
-  return sinon.stub(Class.prototype, functionName).resolves({
-    status,
-    data
-  } as any)
-}
+describe('[package] @ula-aca/schema', () => {
+  describe('[plugin] SchemaController', () => {
+    let eventHandler: EventHandler
+    let schemaPlugin: SchemaController
+    let schemaApiStubbed: sinon.SinonStub
 
-describe('@ula-aca/schema', () => {
-  describe('Schema Plugin', () => {
+    beforeEach(() => {
+      eventHandler = new EventHandler([])
+      schemaPlugin = new SchemaController('http://url.test')
+      schemaPlugin.initialize(eventHandler)
+    })
+
+    afterEach(() => {
+      schemaApiStubbed && schemaApiStubbed.restore()
+    })
+
     it("plugin name should be '@ula-aca/schema/SchemaController'", () => {
-      const acaUrl = 'http://fake'
-      const schemaPlugin = new SchemaController(acaUrl)
-
       schemaPlugin.name.should.equal('@ula-aca/schema/SchemaController')
     })
 
-    describe('handleEvent()', () => {
-      it('should only handle messages it is supposed to', async () => {
-        const eventHandler = new EventHandler([])
-        const acaUrl = 'http://ula.test:7002'
-        const schemaPlugin = new SchemaController(acaUrl)
-
-        schemaPlugin.initialize(eventHandler)
-
-        const ingoreMessageTypes = [
-          'create-shem',
-          'random-message',
-          'a',
-          'get-schema'
-        ]
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const messageType of ingoreMessageTypes) {
-          const message = new Message({
-            type: messageType
-          })
-
-          // eslint-disable-next-line @typescript-eslint/no-empty-function,no-await-in-loop
-          const response = await schemaPlugin.handleEvent(message, () => {})
-
-          response.should.equal('ignored')
-        }
-      })
-
-      it('should throw an error when an unknown message type is passed', async () => {
-        const eventHandler = new EventHandler([])
-        const acaUrl = 'http://ula.test:7002'
-        const schemaPlugin = new SchemaController(acaUrl)
-
-        schemaPlugin.initialize(eventHandler)
-
+    describe('[function] handleEvent()', () => {
+      it("should return 'ignored' when an unknown message type is passed", async () => {
         const ignoreMessageTypes = [
           'create-shem',
           'random-message',
@@ -88,29 +54,22 @@ describe('@ula-aca/schema', () => {
           'get-schema'
         ]
 
-        // eslint-disable-next-line no-restricted-syntax
         for (const messageType of ignoreMessageTypes) {
           const message = new Message({
             type: messageType
           })
 
-          // eslint-disable-next-line @typescript-eslint/no-empty-function,no-await-in-loop
           const response = await schemaPlugin.handleEvent(message, () => {})
 
           response.should.equal('ignored')
         }
       })
 
-      it("should return 'error' when statusCode is not in 200", async () => {
-        const eventHandler = new EventHandler([])
-        const acaUrl = 'http://url.test'
-        const schemaPlugin = new SchemaController(acaUrl)
-        schemaPlugin.initialize(eventHandler)
-
+      it("should return 'error' when statusCode is not in range 200-299", async () => {
         const statusCode = 300
         const expectedResult = 'error'
 
-        const schemaApiStubbed = stubInterfaceFunction({
+        schemaApiStubbed = stubInterfaceFunction({
           Class: SchemaApi,
           functionName: 'schemasCreatedGet',
           status: statusCode
@@ -122,113 +81,28 @@ describe('@ula-aca/schema', () => {
 
         const eventRes = await schemaPlugin.handleEvent(message, () => {})
 
-        schemaApiStubbed.restore()
         eventRes.should.equal(expectedResult)
       })
-    })
 
-    describe('events', () => {
-      const eventHandler = new EventHandler([])
-      const acaUrl = 'http://url.test'
-      const schemaPlugin = new SchemaController(acaUrl)
-      schemaPlugin.initialize(eventHandler)
+      it('should call the callback with the error and statusCode when an API call fails', async () => {
+        const schemaId = 'Non-existent schema id'
+        const data = '500 Internal Server Error\n\nServer got itself in trouble'
 
-      describe('@ula-aca/schema/get-created-schemas', () => {
-        it('should pass the parameters to SchemaApi function schemasCreatedGet', async () => {
-          const schemaId = 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
-          const schemaIssuerDid = 'WgWxqztrNooG92RXvxSTWv'
-          const schemaName = 'schema_name'
-          const schemaVersion = '1.0'
-
-          const data = {
-            schema_ids: ['WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0']
-          }
-          const statusCode = 200
-
-          const expectedResult = new UlaResponse({
-            body: data,
-            statusCode
-          })
-
-          const schemaApiStubbed = stubInterfaceFunction({
-            Class: SchemaApi,
-            functionName: 'schemasCreatedGet',
-            data,
-            status: statusCode
-          })
-
-          const message = new Message({
-            type: '@ula-aca/schema/get-created-schemas',
-            payload: { schemaId, schemaIssuerDid, schemaName, schemaVersion }
-          } as GetCreatedSchemasMessage)
-
-          await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
-            schemaApiStubbed.should.have.been.calledWith(
-              schemaId,
-              schemaIssuerDid,
-              schemaName,
-              schemaVersion
-            )
-            res.should.deep.equal(expectedResult)
-            schemaApiStubbed.restore()
-          })
-        })
-
-        it('should work when no payload is passed', async () => {
-          const data = {
-            schema_ids: ['WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0']
-          }
-          const statusCode = 200
-
-          const expectedResult = new UlaResponse({
-            body: data,
-            statusCode
-          })
-
-          const schemaApiStubbed = stubInterfaceFunction({
-            Class: SchemaApi,
-            functionName: 'schemasCreatedGet',
-            data,
-            status: statusCode
-          })
-
-          const message = new Message({
-            type: '@ula-aca/schema/get-created-schemas'
-          } as GetCreatedSchemasMessage)
-
-          await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
-            schemaApiStubbed.should.have.been.calledWith()
-            res.should.deep.equal(expectedResult)
-            schemaApiStubbed.restore()
-          })
-        })
-      })
-
-      it('@ula-aca/schema/get-schema-by-id', async () => {
-        const schemaId = 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
-
-        const data = {
-          schema_json: {
-            attr_names: ['score'],
-            name: 'schema_name',
-            version: '1.0',
-            ver: '1.0',
-            seqNo: 999,
-            id: 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
-          }
-        }
-        const statusCode = 200
+        const statusCode = 500
 
         const expectedResult = new UlaResponse({
-          body: data,
+          body: {
+            error: data
+          },
           statusCode
         })
 
-        const schemaApiStubbed = stubInterfaceFunction({
+        schemaApiStubbed = stubInterfaceFunction({
           Class: SchemaApi,
           functionName: 'schemasIdGet',
           data,
-          status: statusCode
+          status: statusCode,
+          rejects: true
         })
 
         const message = new Message({
@@ -237,47 +111,153 @@ describe('@ula-aca/schema', () => {
         } as GetSchemaByIdMessage)
 
         await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
-          schemaApiStubbed.should.have.been.calledWith(schemaId)
           res.should.deep.equal(expectedResult)
-          schemaApiStubbed.restore()
         })
       })
 
-      it('@ula-aca/schema/create-schema', async () => {
-        const attributes = ['first_name', 'last_name']
-        const schemaName = 'schema_name'
-        const schemaVersion = '1.0'
+      describe('events', () => {
+        describe('@ula-aca/schema/get-created-schemas', () => {
+          it('should pass the parameters to SchemaApi function schemasCreatedGet', async () => {
+            const schemaId = 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
+            const schemaIssuerDid = 'WgWxqztrNooG92RXvxSTWv'
+            const schemaName = 'schema_name'
+            const schemaVersion = '1.0'
 
-        const data = {
-          schema_id: 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
-        }
-        const statusCode = 200
+            const data = {
+              schema_ids: ['WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0']
+            }
+            const statusCode = 200
 
-        const expectedResult = new UlaResponse({
-          body: data,
-          statusCode
-        })
+            const expectedResult = new UlaResponse({
+              body: data,
+              statusCode
+            })
 
-        const schemaApiStubbed = stubInterfaceFunction({
-          Class: SchemaApi,
-          functionName: 'schemasPost',
-          data,
-          status: statusCode
-        })
+            schemaApiStubbed = stubInterfaceFunction({
+              Class: SchemaApi,
+              functionName: 'schemasCreatedGet',
+              data,
+              status: statusCode
+            })
 
-        const message = new Message({
-          type: '@ula-aca/schema/create-schema',
-          payload: { attributes, schemaName, schemaVersion }
-        } as CreateSchemaMessage)
+            const message = new Message({
+              type: '@ula-aca/schema/get-created-schemas',
+              payload: { schemaId, schemaIssuerDid, schemaName, schemaVersion }
+            } as GetCreatedSchemasMessage)
 
-        await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
-          schemaApiStubbed.should.have.been.calledWith({
-            schema_name: schemaName,
-            schema_version: schemaVersion,
-            attributes
+            await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
+              schemaApiStubbed.should.have.been.calledWith(
+                schemaId,
+                schemaIssuerDid,
+                schemaName,
+                schemaVersion
+              )
+              res.should.deep.equal(expectedResult)
+            })
           })
-          res.should.deep.equal(expectedResult)
-          schemaApiStubbed.restore()
+
+          it('should work when no payload is passed', async () => {
+            const data = {
+              schema_ids: ['WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0']
+            }
+            const statusCode = 200
+
+            const expectedResult = new UlaResponse({
+              body: data,
+              statusCode
+            })
+
+            schemaApiStubbed = stubInterfaceFunction({
+              Class: SchemaApi,
+              functionName: 'schemasCreatedGet',
+              data,
+              status: statusCode
+            })
+
+            const message = new Message({
+              type: '@ula-aca/schema/get-created-schemas'
+            } as GetCreatedSchemasMessage)
+
+            await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
+              schemaApiStubbed.should.have.been.calledWith()
+              res.should.deep.equal(expectedResult)
+            })
+          })
+        })
+
+        it('@ula-aca/schema/get-schema-by-id', async () => {
+          const schemaId = 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
+
+          const data = {
+            schema_json: {
+              attr_names: ['score'],
+              name: 'schema_name',
+              version: '1.0',
+              ver: '1.0',
+              seqNo: 999,
+              id: 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
+            }
+          }
+          const statusCode = 200
+
+          const expectedResult = new UlaResponse({
+            body: data,
+            statusCode
+          })
+
+          schemaApiStubbed = stubInterfaceFunction({
+            Class: SchemaApi,
+            functionName: 'schemasIdGet',
+            data,
+            status: statusCode
+          })
+
+          const message = new Message({
+            type: '@ula-aca/schema/get-schema-by-id',
+            payload: { schemaId }
+          } as GetSchemaByIdMessage)
+
+          await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
+            schemaApiStubbed.should.have.been.calledWith(schemaId)
+            res.should.deep.equal(expectedResult)
+          })
+        })
+
+        it('@ula-aca/schema/create-schema', async () => {
+          const attributes = ['first_name', 'last_name']
+          const schemaName = 'schema_name'
+          const schemaVersion = '1.0'
+
+          const data = {
+            schema_id: 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0'
+          }
+          const statusCode = 200
+
+          const expectedResult = new UlaResponse({
+            body: data,
+            statusCode
+          })
+
+          schemaApiStubbed = stubInterfaceFunction({
+            Class: SchemaApi,
+            functionName: 'schemasPost',
+            data,
+            status: statusCode
+          })
+
+          const message = new Message({
+            type: '@ula-aca/schema/create-schema',
+            payload: { attributes, schemaName, schemaVersion }
+          } as CreateSchemaMessage)
+
+          await schemaPlugin.handleEvent(message, (res: UlaResponse) => {
+            schemaApiStubbed.should.have.been.calledWith({
+              schema_name: schemaName,
+              schema_version: schemaVersion,
+              attributes
+            })
+            res.should.deep.equal(expectedResult)
+          })
         })
       })
     })
