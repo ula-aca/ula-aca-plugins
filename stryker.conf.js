c86dceff6a6f7dc95e689cb22ff6793813797ff2
Timo Glastra <timo@glastra.me>
2020-01-14 19:24:07 +0100

    build: fixed tsconfig build setup

--- stryker.conf.js
+++ stryker.conf.js
@@ -8,7 +8,7 @@ module.exports = config => {
     mochaOptions: {
       config: '.mocharc.unit.json'
     },
-    tsconfigFile: 'tsconfig.json',
+    tsconfigFile: 'tsconfig.test.json',
     mutate: ['packages/**/src/**/*.ts'],
     thresholds: {
       high: 100,
