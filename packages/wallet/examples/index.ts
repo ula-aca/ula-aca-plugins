6ef2ea7ad8aa377711f02643e84fbbd67e6582c0
Karim Stekelenburg <karim.stekelenburg@student.hu.nl>
2020-01-16 00:40:23 +0100

    feat(wallet): added examples

---
+++ packages/wallet/examples/index.ts
@@ -0,0 +1,143 @@
+import { EventHandler, UlaResponse } from 'universal-ledger-agent'
+
+import {
+  GetDidsBody,
+  GetDidsResult,
+  GetDidsMessage,
+  WalletMessageTypes,
+  CreateLocalDidResult,
+  CreateLocalDidMessage,
+  FetchPublicDidResult,
+  FetchPublicDidMessage,
+  AssignPublicDidBody,
+  AssignPublicDidResult,
+  AssignPublicDidMessage,
+  GetTagPolicyBody,
+  GetTagPolicyResult,
+  GetTagPolicyMessage,
+  SetTagPolicyBody,
+  SetTagPolicyMessage
+} from '../src'
+
+async function getDids(
+  eventHandler: EventHandler,
+  options: GetDidsBody
+): Promise<GetDidsResult> {
+  return new Promise((resolve, reject) => {
+    const message: GetDidsMessage = {
+      type: WalletMessageTypes.GET_DIDS,
+      body: options
+    }
+
+    eventHandler.processMsg(message, (response: UlaResponse) => {
+      if (response.statusCode < 200 || response.statusCode >= 300)
+        reject(response.body)
+
+      const result: GetDidsResult = response.body
+      resolve(result)
+    })
+  })
+}
+
+async function createLocalDid(
+  eventHandler: EventHandler
+): Promise<CreateLocalDidResult> {
+  return new Promise((resolve, reject) => {
+    const message: CreateLocalDidMessage = {
+      type: WalletMessageTypes.CREATE_LOCAL_DID
+    }
+
+    eventHandler.processMsg(message, (response: UlaResponse) => {
+      if (response.statusCode < 200 || response.statusCode >= 300)
+        reject(response.body)
+
+      const result: CreateLocalDidResult = response.body
+      resolve(result)
+    })
+  })
+}
+
+async function fetchPublicDid(
+  eventHandler: EventHandler
+): Promise<FetchPublicDidResult> {
+  return new Promise((resolve, reject) => {
+    const message: FetchPublicDidMessage = {
+      type: WalletMessageTypes.FETCH_PUBLIC_DID
+    }
+
+    eventHandler.processMsg(message, (response: UlaResponse) => {
+      if (response.statusCode < 200 || response.statusCode >= 300)
+        reject(response.body)
+
+      const result: FetchPublicDidResult = response.body
+      resolve(result)
+    })
+  })
+}
+
+async function assignPublicDid(
+  eventHandler: EventHandler,
+  options: AssignPublicDidBody
+): Promise<AssignPublicDidResult> {
+  return new Promise((resolve, reject) => {
+    const message: AssignPublicDidMessage = {
+      type: WalletMessageTypes.ASSIGN_PUBLIC_DID,
+      body: options
+    }
+
+    eventHandler.processMsg(message, (response: UlaResponse) => {
+      if (response.statusCode < 200 || response.statusCode >= 300)
+        reject(response.body)
+
+      const result: AssignPublicDidResult = response.body
+      resolve(result)
+    })
+  })
+}
+
+async function getTagPoliccy(
+  eventHandler: EventHandler,
+  options: GetTagPolicyBody
+): Promise<GetTagPolicyResult> {
+  return new Promise((resolve, reject) => {
+    const message: GetTagPolicyMessage = {
+      type: WalletMessageTypes.GET_TAG_POLICY,
+      body: options
+    }
+
+    eventHandler.processMsg(message, (response: UlaResponse) => {
+      if (response.statusCode < 200 || response.statusCode >= 300)
+        reject(response.body)
+
+      const result: GetTagPolicyResult = response.body
+      resolve(result)
+    })
+  })
+}
+
+async function setTagPolicy(
+  eventHandler: EventHandler,
+  options: SetTagPolicyBody
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const message: SetTagPolicyMessage = {
+      type: WalletMessageTypes.SET_TAG_POLICY,
+      body: options
+    }
+
+    eventHandler.processMsg(message, (response: UlaResponse) => {
+      if (response.statusCode < 200 || response.statusCode >= 300)
+        reject(response.body)
+      resolve()
+    })
+  })
+}
+
+export {
+  getDids,
+  createLocalDid,
+  fetchPublicDid,
+  assignPublicDid,
+  getTagPoliccy,
+  setTagPolicy
+}
