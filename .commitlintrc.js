83f55cf33ce1404e640468f4b85f04e4953e154e
Timo Glastra <timo@glastra.me>
2020-04-20 14:05:36 +0200

    chore: make max-header-length in commitlint a warning
    dependabot sometimes creates commit messages that violate this rule. nothing we can do about it at
    the moment.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,7 @@ const { utils } = require('@commitlint/config-lerna-scopes')
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-max-length': [1, 'always', 72],
     'scope-case': [2, 'always', 'kebab-case'],
     'scope-enum': ctx =>
       utils
@@ -10,7 +11,13 @@ module.exports = {
         .then(packages => [
           2,
           'always',
-          [...packages, 'deps-dev', 'deps', 'aca-webhook-event-models', 'release']
+          [
+            ...packages,
+            'deps-dev',
+            'deps',
+            'aca-webhook-event-models',
+            'release'
+          ]
         ])
   }
 }
